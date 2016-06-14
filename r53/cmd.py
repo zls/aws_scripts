9714778dbc6859e2472f3967abb7c2b1fcb89bba
Zachary Softich <zsoftich@fisdap.net>
2016-06-14 10:37:40 -0500

    Add -z option to append zone name to RR name.
    Cleanup help section.

--- r53/cmd.py
+++ r53/cmd.py
@@ -9,6 +9,10 @@ import zones
 import rrsets
 import func
 
+EXIT_OK = 0
+EXIT_PENDING = 1
+EXIT_BAD_OPT = 10
+
 CTX_SETTINGS = {'help_option_names': ['-h', '--help']}
 
 @click.group(context_settings=CTX_SETTINGS)
@@ -22,46 +26,75 @@ def cmd_set():
 
 
 @cmd_set.command(name='weight', context_settings=CTX_SETTINGS)
-@click.option('--verbose', '-v', is_flag=True)
-@click.option('--dry-run', '-n', is_flag=True)
-@click.option('--append-domain', '-a')
-@click.option('--append-to-set-id', '-s')
-@click.option('--name-as-set-id-prefix', '-p', is_flag=True, default=False)
+@click.option('--verbose', '-v', is_flag=True, help='Print out change batch')
+@click.option('--dry-run', '-n', is_flag=True, help='Do not apply change batch, implies -v')
+@click.option('--append-domain', '-a', help='Append domain name to RR names')
+@click.option('--use-zone-name', '-z', is_flag=True, help='Append zone name to RR names')
+@click.option('--append-to-set-id', '-s', help='Append value to SetIdentifiers')
+@click.option('--name-as-set-id-prefix', '-p', is_flag=True, default=False, help='Use the RR name value as the SetIdentifier')
 @click.argument('weight', type=click.INT)
 @click.argument('zone')
-@click.argument('name', metavar='<name|set_id>', nargs=-1)
+@click.argument('name', metavar='NAME [NAME...]', nargs=-1)
 def cmd_set_weight(**kwargs):
+    '''
+    Arguments:
+
+    \b
+        weight: The value to set the weight to.
+        zone:   AWS Hosted Zone that record is found in.
+        name:   The name of the resource record (RR) to operate on. Use a pipe
+                character '|' to separate the NAME|SET_ID portions.
+                Ex. 'www.example.com|www-frontlb01'
+
+    Example:
+
+        r53 set weight -pz -s '-frontlb01' 0 example.com www api
+
+    '''
     chgs = []
     zid = zones.find_zone_id(kwargs['zone'])
     rrs = rrsets.get_rrset(zid)
     for name in kwargs['name']:
+
         if kwargs['name_as_set_id_prefix']:
             nam, sid = name, name
         else:
             nam, sid = name.split('|')
-        if kwargs['append_domain']:
+
+        if kwargs['append_domain'] and kwargs['use_zone_name']:
+            click.echo('ERROR: --append-domain and --use-zone-name are mutually exclusive options.',
+                       err=True)
+            sys.exit(EXIT_BAD_OPT)
+        elif kwargs['append_domain']:
             nam = func.append_domain(nam, kwargs['append_domain'])
+        elif kwargs['use_zone_name']:
+            nam = func.append_domain(nam, kwargs['zone'])
+
         if kwargs['append_to_set_id']:
             sid += kwargs['append_to_set_id']
+
         nam = func.add_root_label(nam)
         for rr in rrs:
             if rr['Name'] == nam and rr['SetIdentifier'] == sid:
                 rr['Weight'] = kwargs['weight']
                 chgs.append({'Action': 'UPSERT', 'ResourceRecordSet': rr})
+
     if chgs:
         cb = rrsets.change_batch(chgs)
         if kwargs['verbose'] or kwargs['dry_run']:
             pprint.pprint(cb)
+
         if kwargs['dry_run']:
-            print('Dry run only, exiting now.')
-            sys.exit(0)
+            click.echo('Dry run only, exiting now.')
+            sys.exit(EXIT_OK)
+
         resp = rrsets.apply_changebatch(zid, cb)
         if not func.change_insync(resp):
-            print("Changes are still not insync, please verify")
-            sys.exit(1)
+            click.echo('Changes are still not insync, please verify', err=True)
+            sys.exit(EXIT_PENDING)
         else:
-            print("All changes have been applied")
-            sys.exit(0)
+            click.echo('All changes have been applied')
+            sys.exit(EXIT_OK)
 
 
 @cmd_cli.group(name='list', context_settings=CTX_SETTINGS)
